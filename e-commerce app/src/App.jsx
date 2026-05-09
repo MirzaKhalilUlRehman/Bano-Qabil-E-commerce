5d8af4a0c989e31e27dc5efc1ebf74f0766a35f5
Mirza Khalil <mirzakhalilulrehman123456@gamil.com>
2026-05-09 18:14:42 +0500

    complete scroll section

--- e-commerce app/src/App.jsx
+++ e-commerce app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ScrollProducts from './components/ScrollProducts'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <ScrollProducts/>
     </div>
   )
 }
--- e-commerce app/src/components/ScrollProducts.jsx
+++ e-commerce app/src/components/ScrollProducts.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const ScrollProducts = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [product, setProduct] = useState([]);
 
-export default ScrollProducts
+  const FetchProduct = async () => {
+    const response = await fetch(
+      "https://dummyjson.com/products"
+    );
+
+    const data = await response.json();
+
+
+     setProduct(data.products);
+  };
+
+  useEffect(() => {
+    FetchProduct();
+  }, []);
+
+  return (
+    <section className="flex flex-col w-[80%] m-auto mt-28">
+      <h1 className="text-4xl font-bold">Top Products</h1>
+
+      <div className="flex gap-8 overflow-x-scroll mt-8 no-scrollbar ">
+        {product.map((item) => (
+          <div key={item.id} className=" max-w-70 min-w-70 h-80 flex flex-col bg-wild-sand items-center justify-center whitespace-nowrap p-4 rounded-lg "> 
+            <img src={item.thumbnail} alt={item.title} className=" w-full h-full object-fill " />
+            <span>{item.title}</span>
+            <span className=" font-medium  "> ${item.price}</span>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ScrollProducts;
